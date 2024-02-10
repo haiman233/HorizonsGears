df1e1f90be44f3d3afdb7bbc75ae8f6829fcf817
Gidor <60816479+xGidor@users.noreply.github.com>
2024-02-10 14:16:53 +0100

    feat: added horizon armour set.

--- src/main/java/me/gidorstudios/horizonsgears/HorizonsGears.java
+++ src/main/java/me/gidorstudios/horizonsgears/HorizonsGears.java
@@ -31,18 +31,57 @@ public class HorizonsGears extends JavaPlugin implements SlimefunAddon {
 
         SlimefunItemStack CrystalitemStack = new SlimefunItemStack("HORIZON_CRYSTAL", Material.DIAMOND, "&7Horizon Crystal", "", "&3A shiny gem created by insane pressures.");
 
-        // The custom item for our SlimefunItem
-        SlimefunItemStack HorizonSworditemStack = new SlimefunItemStack("HORIZON_SWORD", Material.DIAMOND_SWORD, "&bHorizon &3Sword", "", "&7A powerful sword enchanted deep in the sea.");
-
+        SlimefunItemStack HorizonSworditemStack = new SlimefunItemStack("HORIZON_SWORD", Material.DIAMOND_SWORD, "&bHorizons &3Sword", "", "&7A powerful sword enchanted deep in the sea.");
         HorizonSworditemStack.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, 7);
         HorizonSworditemStack.addUnsafeEnchantment(Enchantment.DURABILITY, 7);
         HorizonSworditemStack.addUnsafeEnchantment(Enchantment.FIRE_ASPECT, 2);
 
+        SlimefunItemStack HorizonHelmetitemStack = new SlimefunItemStack("HORIZON_HELMET", Material.DIAMOND_HELMET, "&bHorizons &3Helmet", "", "&7A powerful helmet crafted deep within the nether.");
+        HorizonHelmetitemStack.addUnsafeEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 7);
+        HorizonHelmetitemStack.addUnsafeEnchantment(Enchantment.DURABILITY, 7);
+        HorizonHelmetitemStack.addUnsafeEnchantment(Enchantment.THORNS, 2);
+
+        SlimefunItemStack HorizonChestitemStack = new SlimefunItemStack("HORIZON_CHESTPLATE", Material.DIAMOND_CHESTPLATE, "&bHorizons &3Chestplate", "", "&7A powerful chestplate crafted deep within the nether.");
+        HorizonChestitemStack.addUnsafeEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 7);
+        HorizonChestitemStack.addUnsafeEnchantment(Enchantment.DURABILITY, 7);
+
+        SlimefunItemStack HorizonLeggingsitemStack = new SlimefunItemStack("HORIZON_LEGGINGS", Material.DIAMOND_LEGGINGS, "&bHorizons &3Leggings", "", "&7Powerful leggings crafted deep within the nether.");
+        HorizonLeggingsitemStack.addUnsafeEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 7);
+        HorizonLeggingsitemStack.addUnsafeEnchantment(Enchantment.DURABILITY, 7);
+
+        SlimefunItemStack HorizonBootsitemStack = new SlimefunItemStack("HORIZON_BOOTS", Material.DIAMOND_BOOTS, "&bHorizons &3Boots", "", "&7Powerful boots crafted deep within the nether.");
+        HorizonBootsitemStack.addUnsafeEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 7);
+        HorizonBootsitemStack.addUnsafeEnchantment(Enchantment.DURABILITY, 7);
+        HorizonBootsitemStack.addUnsafeEnchantment(Enchantment.PROTECTION_FALL, 2);
 
         ItemStack[] cyrstalRecipe = {
                 SlimefunItems.SYNTHETIC_DIAMOND,   new ItemStack(Material.DIAMOND),      SlimefunItems.CARBON_CHUNK,
-                null,                               null,                                      null,
+                SlimefunItems.CARBONADO,           SlimefunItems.SYNTHETIC_SAPPHIRE,     SlimefunItems.DAMASCUS_STEEL_INGOT,
+                null,                              null,                                 null
+        };
+
+        ItemStack[] helmetRecipe = {
+                CrystalitemStack,                  CrystalitemStack,                           CrystalitemStack,
+                CrystalitemStack,                  null,                                       CrystalitemStack,
                 null,                              null,                                       null
+        };
+
+        ItemStack[] chestRecipe = {
+                CrystalitemStack,                  null,                                       CrystalitemStack,
+                CrystalitemStack,                  CrystalitemStack,                           CrystalitemStack,
+                CrystalitemStack,                  CrystalitemStack,                           CrystalitemStack
+        };
+
+        ItemStack[] leggingsRecipe = {
+                CrystalitemStack,                  CrystalitemStack,                           CrystalitemStack,
+                CrystalitemStack,                  null,                                       CrystalitemStack,
+                CrystalitemStack,                  null,                                       CrystalitemStack
+        };
+
+        ItemStack[] bootsRecipe = {
+                null,                              null,                                       null,
+                CrystalitemStack,                  null,                                       CrystalitemStack,
+                CrystalitemStack,                  null,                                       CrystalitemStack
         };
 
         // A 3x3 shape representing our recipe
@@ -52,8 +91,21 @@ public class HorizonsGears extends JavaPlugin implements SlimefunAddon {
                 null,                              new ItemStack(Material.STICK),      null
         };
 
+        SlimefunItem sfHItem = new SlimefunItem(itemGroup, HorizonHelmetitemStack, RecipeType.ENHANCED_CRAFTING_TABLE, helmetRecipe);
+        sfHItem.register(this);
+
+        SlimefunItem sfChItem = new SlimefunItem(itemGroup, HorizonChestitemStack, RecipeType.ENHANCED_CRAFTING_TABLE,  chestRecipe);
+        sfChItem.register(this);
+
+        SlimefunItem sfLItem = new SlimefunItem(itemGroup, HorizonLeggingsitemStack, RecipeType.ENHANCED_CRAFTING_TABLE, leggingsRecipe);
+        sfLItem.register(this);
+
+        SlimefunItem sfBItem = new SlimefunItem(itemGroup, HorizonBootsitemStack, RecipeType.ENHANCED_CRAFTING_TABLE, bootsRecipe);
+        sfBItem.register(this);
+
         SlimefunItem sfCItem = new SlimefunItem(itemGroup, CrystalitemStack, RecipeType.PRESSURE_CHAMBER, cyrstalRecipe);
         sfCItem.register(this);
+
         SlimefunItem sfItem = new SlimefunItem(itemGroup, HorizonSworditemStack, RecipeType.ENHANCED_CRAFTING_TABLE, swordRecipe);
         sfItem.register(this);
 
@@ -66,6 +118,14 @@ public class HorizonsGears extends JavaPlugin implements SlimefunAddon {
         Research researchSword = new Research(researchKeySword, 701, "Unlocks the horizon sword.", 15);
         researchSword.addItems(HorizonSworditemStack);
         researchSword.register();
+
+        NamespacedKey researchKeyArmour = new NamespacedKey(this, "harmour_research");
+        Research researchArmour = new Research(researchKeyArmour, 702, "Unlocks the horizon sword.", 50);
+        researchArmour.addItems(HorizonHelmetitemStack);
+        researchArmour.addItems(HorizonChestitemStack);
+        researchArmour.addItems(HorizonLeggingsitemStack);
+        researchArmour.addItems(HorizonBootsitemStack);
+        researchArmour.register();
         // Our item is now registered
     }
 
